377a0355df3061a0fd3cbc36d670bfa03871940d
1893069 <1893069@E21052.CEGEPGIM.LCL>
2019-02-17 09:40:50 -0500

    Set method

---
+++ src/falcon/flights/Certification.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package falcon.flights;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1893069
+ */
+public class Certification 
+{
+Date dateofcertification;    
+public void Certification()
+{
+this.dateofcertification=dateofcertification;
+}
+public Date getdateofcertification()
+{
+    return dateofcertification;
+}
+public void setdateofcertification()
+{
+    this.dateofcertification=dateofcertification;
+}
+
+}
+
