dcabd45458de1d36f7f8b4ae77a125ddfa93e4e7
1893541 <1893541@E21054.CEGEPGIM.LCL>
2019-03-03 09:12:00 -0500

    Merge origin/master
    Conflicts:
    	src/falconflights/Flight.java

--- src/falconflights/Agent.java
+++ src/falconflights/Agent.java
@@ -5,6 +5,10 @@
  */
 package falconflights;
 
+import static falconflights.Booking.dateType;
+import static falconflights.Booking.generateID;
+import java.util.Date;
+
 /**
  *
  * @author 1893541
@@ -12,10 +16,51 @@ package falconflights;
 public class Agent extends Employee{
     Details details1=new Details();
     String Gender;
-    public static String Agent(String Gender){
-        //this.Gender=Gender;
-        return Gender;
+    String Id;
+    Date date;
+    String ID_CODE;
+    public Agent(String Id1,String Gender1,Date date1){
+        Id=Id1;
+        Gender=Gender1;
+        date=date1;
+        String y=date.toString();
+         y=y.toUpperCase();
+         Id=Id.toUpperCase();
+         char c=dateType(y);
+         ID_CODE= generateID(Id,y,c);
+         Display();
+       
     }
+    
+     public static char dateType(String y){//EVEN OR ODD
+       char c1=y.charAt(9);
+     int b=Character.getNumericValue(c1);
+     if(b%2==0){
+        c1='E';
+          }else{
+        c1='O';
+    } 
+     return c1;
+   }
+   public static String generateID(String Fname,String y,char c1){
+       String u;
+        if("".equals(Fname)){
+           u=""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+"XX";
+    // System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+"XX");
+        }else if(Fname.length()==1){
+            u=""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+"X";
+           // System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+"X");
+        }else{
+            u=""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+Fname.charAt(Fname.length()-1);
+           // System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+Fname.charAt(Fname.length()-1));
+        }
+        return u;
+    }
+   public void Display(){
+       System.out.println(ID_CODE);
+       System.out.println("Id:"+Id+"\nGender:"+Gender);
+   }
+
     public void setGender(String Gender){
         this.Gender=Gender;
     }
--- src/falconflights/FalconFlights.java
+++ src/falconflights/FalconFlights.java
@@ -13,67 +13,19 @@ import java.util.Scanner;
  * @author 1896052
  */
 public class FalconFlights {
-   
-
-  /*  public static void Display(String Fname,String Lname,String y,char c1){
-        if("".equals(Fname) && "".equals(Lname)){
-            System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+"XX");
-        }else if("".equals(Lname) && !"".equals(Fname)){
-            System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+"X");
-        }else{
-            System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+Lname.charAt(0));
-        }
-    }*/
-    /**
-     * @param args the command line arguments
-     */
       
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    Agent agent1=new Agent();
+    Agent agent1=new Agent("","male",new Date());
 //    Booking booking1=new Booking();
     Details details1=new Details();
     Employee employee1=new Employee();
-    Flight flight1=new Flight("N","montreal","india",new Date());
+    Flight flight1=new Flight("Nishanth","montreal","india",new Date());
     Plane plane1=new Plane();
     person person1=new person();
     Certification certification1=new Certification();
     Pilot p1=new Pilot();
-    Customer customer1=new Customer();
-    
-    
-    
-        // TODO code application logic here
-   
-   // plane1.modelnumber="sajhdkj";
-   // details1.Details("adfad");
-    //details1.setRole("asjgdh");
-    //System.out.println(details1.getRole());
-   /* String ID;
-    String f,l;
-    Date de = new Date();
-    String y=de.toString();
-    y=y.toUpperCase();
-    System.out.println(y);
-    String Fname="",Lname="";
-    
-        System.out.println("Enter Your Name");
-        f=sc.next();
-        f=f.toUpperCase();
-       /* System.out.println("Enter Last Name");
-        l=sc.next();*/
- /*   char c1 = y.charAt(8);
-    char c2=y.charAt(9);
-   int a=Character.getNumericValue(c1);
-   int b=Character.getNumericValue(c2);
-    if(b%2==0){
-        c1='E';
-          }else{
-        c1='O';
-    } 
-    Fname=f;*/
-    //Lname=l;
-    //String x="19-FEB-OD-XX";
-  //  Display(Fname,y,c1);
+//    Customer customer1=new Customer();
+    Booking book1=new Booking("",new Date());
     }
 }
