158132f8b22cbce20479321ebb0c62ee9f514483
1893541 <1893541@E21054.CEGEPGIM.LCL>
2019-03-03 09:26:08 -0500

    Merge origin/master
    Conflicts:
    	src/falconflights/Flight.java

--- src/falconflights/FalconFlights.java
+++ src/falconflights/FalconFlights.java
@@ -16,16 +16,14 @@ public class FalconFlights {
       
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    Agent agent1=new Agent("","male",new Date());
-//    Booking booking1=new Booking();
-    Details details1=new Details();
+    Agent agent1=new Agent("","male",new Date());  
     Employee employee1=new Employee();
     Flight flight1=new Flight("Nishanth","montreal","india",new Date());
     Plane plane1=new Plane();
     person person1=new person();
     Certification certification1=new Certification();
     Pilot p1=new Pilot();
-//    Customer customer1=new Customer();
     Booking book1=new Booking("",new Date());
+     Details details1=new Details("","Supervisor",new Date());
     }
 }
