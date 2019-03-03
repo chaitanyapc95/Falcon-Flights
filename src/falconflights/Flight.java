38a07f97a64e5a57e5283a5c91f75f7b0db3fa6f
1893541 <1893541@E21054.CEGEPGIM.LCL>
2019-03-03 08:41:05 -0500

    Merge origin/master
    Conflicts:
    	src/falcon/flights/FalconFlights.java
    	src/falcon/flights/Flight.java
    	src/falcon/flights/Pilot.java
    	src/falcon/flights/Plane.java

--- src/falconflights/Flight.java
+++ src/falconflights/Flight.java
@@ -3,9 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package falconflights;
 
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.String;
 import java.sql.Time;
 import java.util.Date;
 
@@ -14,54 +18,57 @@ import java.util.Date;
  * @author 1893541
  */
 public class Flight {
-
-  
-    
+   
     Plane plane1=new Plane();
     Details details1=new Details();
     String Id;
     String origin;
     String destination;
     Date traveldate;
-    Time time;
+    String ID_CODE;
+  //  Time time;
     
-    
-   public Flight(String Id,String origin,String destination,Date traveldate){
-       Id=Id;
-       origin=origin;
-       destination=destination;
-       traveldate=traveldate;
-      
-    DisplayFlight(Id,origin,destination,traveldate);
+   public Flight(String Id1,String origin1,String destination1,Date traveldate1){
+       Id=Id1;
+       origin=origin1;
+       destination=destination1;
+       traveldate=traveldate1;
+     //  this.time=time;
+     String y=traveldate.toString();
+         y=y.toUpperCase();
+      char c=dateType(y);
+        ID_CODE= generateID(Id,y,c);
+         Display();
    }
-  
-   public static String DisplayFlight(String Id,String origin,String destination,Date traveldate){
-    String f,l;
-    String y=traveldate.toString();
-    y=y.toUpperCase();
-        f=Id.toUpperCase();  
-    char c1 = y.charAt(8);
-    char c2=y.charAt(9);
-   int a=Character.getNumericValue(c1);
-   int b=Character.getNumericValue(c2);
-    if(b%2==0){
+   public static char dateType(String y){//EVEN OR ODD
+       char c1=y.charAt(9);
+     int b=Character.getNumericValue(c1);
+     if(b%2==0){
         c1='E';
           }else{
         c1='O';
     } 
-    String Fname=f;
-    if("".equals(Fname)){
-            System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+"XX");
+     return c1;
+   }
+   public static String generateID(String Fname,String y,char c1){
+       String u;
+        if("".equals(Fname)){
+           u=""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+"XX";
+    // System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+"XX");
+        }else if(Fname.length()==1){
+            u=""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+"X";
+           // System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+"X");
         }else{
-            System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+Fname.charAt(Fname.length()-1));
+            u=""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+Fname.charAt(Fname.length()-1);
+           // System.out.println(""+y.charAt(26)+y.charAt(27)+"-"+y.charAt(4)+y.charAt(5)+y.charAt(6)+"-"+c1+"-"+Fname.charAt(0)+Fname.charAt(Fname.length()-1));
         }
-       
-    System.out.println(Id+"/n"+origin+"/n"+destination+"/n"+traveldate);
-    return Id+origin+destination+traveldate;
-      }
+        return u;
+    }
+   public void Display(){
+       System.out.println(ID_CODE);
+       System.out.println("Id:"+Id+"/norigin:"+origin+"/nDestination:"+destination+"/nTravelDate:"+traveldate);
+   }
 
-
-    
     
    public String getId(){
        return Id;
@@ -75,9 +82,9 @@ public class Flight {
    public Date getDate(){
        return traveldate;
    }
-   public Time getTime(){
-       return time;
-   }
+//   public Time getTime(){
+//       return time;
+//   }
    public void setId(String Id){
        this.Id=Id;
    }
@@ -90,8 +97,4 @@ public class Flight {
    public void setTravelDate(Date traveldate){
        this.traveldate=traveldate;
    }
-   public void setTime(Time time){
-       this.time=time;
-   }
-
 }
